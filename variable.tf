1107e74323c1f3caf02db7b7a0bae6bd055457eb
mansuri-naushad <121010963+mansuri-naushad@users.noreply.github.com>
2023-05-04 17:29:08 +0530

    Add files via upload

---
+++ variable.tf
@@ -0,0 +1,27 @@
+variable "rds_flavor" {
+  default = "rds.mysql.c6.large.2"
+}
+
+variable "rds_mysql_version" {
+  default = "8.0"
+}
+
+variable "rds_port" {
+  default = 3306
+}
+
+variable "rds_password" {
+  default = "Fake@2020Pass"
+}
+
+variable vpc_id {
+   default = "will be apply"
+}
+
+variable subnet_id {
+   default = "will be apply"
+}
+
+variable vpc_cidr {
+   default = "will be apply"
+}
